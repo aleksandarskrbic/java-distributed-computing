676c3db0cbaff27c484fadfd75842af04e12fd53
Aleksandar Skrbic <skrbic.alexa@gmail.com>
2022-10-26 15:43:07 +0200

    start autohealer

--- auto-healer/src/main/java/io/github/aleksandar/Main.java
+++ auto-healer/src/main/java/io/github/aleksandar/Main.java
@@ -1,7 +1,21 @@
 package io.github.aleksandar;
 
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException, KeeperException, IOException {
+        if (args.length != 2) {
+            System.out.println("Expecting parameters <number of workers> <path to worker jar file>");
+            System.exit(1);
+        }
+
+        int numberOfWorkers = Integer.parseInt(args[0]);
+        var pathToWorkerProgram = args[1];
+        var autoHealer = new AutoHealer(numberOfWorkers, pathToWorkerProgram);
+        autoHealer.startWatchingWorkers();
+        autoHealer.run();
+        autoHealer.close();
     }
 }
